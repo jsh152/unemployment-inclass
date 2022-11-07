2e30f319d191e969110ee30e1e4f89c8c0a5c4ff
jsh152 <112024543+jsh152@users.noreply.github.com>
2022-11-07 17:55:26 -0500

    Refactored and tested stocks data fetching logic

--- app/stocks.py
+++ app/stocks.py
@@ -9,15 +9,22 @@ from app.alpha import API_KEY
 def format_usd(my_price):
     return f"${my_price:,.2f}"
 
+def fetch_stocks_data(symbol):
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
+
+    df = read_csv(request_url)
+
+    return df
+
+
 if __name__ == "__main__":
 
 
     symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
     print("SYMBOL:", symbol)
 
-    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
+    df = fetch_stocks_data(symbol)
 
-    df = read_csv(request_url)
     print(df.columns)
     print(df.head())
     #breakpoint()
--- test/stocks_test.py
+++ test/stocks_test.py
@@ -1,6 +1,8 @@
 
 from app.stocks import format_usd
+from app.stocks import fetch_stocks_data
 
+from pandas import DataFrame
 
 
 def test_usd_formatting():
@@ -10,3 +12,18 @@ def test_usd_formatting():
     assert format_usd(4.5) == "$4.50"
     assert format_usd(4.5) == "$4.50"
     assert format_usd(4.5555555) == "$4.56"
+
+
+def test_data_fetching():
+    result = fetch_stocks_data("NFlX")
+    assert isinstance(result, DataFrame)
+
+    assert "timestamp" in result.columns
+    assert "adjusted_close" in result.columns
+    assert "high" in result.columns
+    assert "low" in result.columns
+
+
+    assert len(result) >= 100
+
+
